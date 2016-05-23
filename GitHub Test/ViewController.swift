ba68c59696ac7b42e73427e0714f811d50bd91b3
CKFlawless <ckflawless@gmail.com>
2016-05-23 14:38:57 -0400

    Update ViewController.swift

--- GitHub Test/ViewController.swift
+++ GitHub Test/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     
     let userName = "CKFlawless"
+    //comment line here
 
     override func viewDidLoad() {
         super.viewDidLoad()
